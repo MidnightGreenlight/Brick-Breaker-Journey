9569d67b75875d91f19329cc7637bf258e0eb3f6
Midnight Greenlight <midlyte@protonmail.com>
2021-05-17 17:14:00 +0200

    Add brick

--- ball/ball.gd
+++ ball/ball.gd
@@ -7,4 +7,8 @@ func _ready() -> void:
 	velocity = Vector2.UP.rotated(-PI/4) * speed
 
 func _process(delta:float) -> void:
-	move_and_collide(velocity * delta)
+	var collision := move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+		if collision.collider.has_method("hit"):
+			collision.collider.hit()
